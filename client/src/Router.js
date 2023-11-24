4051d04665b5df2933c7c72a95cf756cbd7977b4
soma <macaronieyes@gmail.com>
2023-11-24 22:54:24 +0900

    feat: router

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,11 +1,13 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import RootLayout from "./pages/Root";
+import PostPage from "./pages/Post";
 
 const Router = () => {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <RootLayout />,
+      children: [{ index: true, element: <PostPage /> }],
     },
   ]);
 
