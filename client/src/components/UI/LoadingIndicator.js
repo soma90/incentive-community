e2438f7942d696cf7d04263c93d571b405b9ffe3
soma <macaronieyes@gmail.com>
2023-12-26 22:48:45 +0900

    feat: loading indicator

---
+++ client/src/components/UI/LoadingIndicator.js
@@ -0,0 +1,14 @@
+import styles from "./LoadingIndicator.module.css";
+
+const LoadingIndicator = () => {
+  return (
+    <div className={styles["lds-ring"]}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default LoadingIndicator;
