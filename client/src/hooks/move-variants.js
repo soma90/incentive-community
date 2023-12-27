a95f6794ce9c074b79451de974302be1ea5036b5
soma <macaronieyes@gmail.com>
2023-12-27 23:33:34 +0900

    feat: 팝업 애니메이션 use hook

---
+++ client/src/hooks/move-variants.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+const useMoveVariants = () => {
+  const [variants, setVariants] = useState({});
+  const [clickedElement, setClickedElement] = useState(null);
+
+  const variantsClickHandler = ({
+    element = null,
+    openVariant = {},
+    closeVariant = {},
+  }) => {
+    setClickedElement(element);
+    if (!element) {
+      setVariants({});
+      return;
+    }
+
+    const height = element.offsetHeight;
+    const width = element.offsetWidth;
+    const rect = element.getBoundingClientRect();
+    const x = rect.left;
+    const y = rect.top;
+    element.style.opacity = 0;
+
+    setVariants({
+      variants: {
+        open: {
+          top: 0,
+          left: "calc(50% - var(--screen-width-1)/2)",
+          height: "100%",
+          width: "var(--screen-width-1)",
+          ...openVariant,
+        },
+        closed: {
+          top: y,
+          left: x,
+          height,
+          width,
+          ...closeVariant,
+        },
+      },
+    });
+  };
+
+  return { variants, variantsClickHandler, clickedElement };
+};
+
+export default useMoveVariants;
