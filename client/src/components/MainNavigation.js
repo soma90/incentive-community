077c6a7c23e732778743afe778e4705e1241bf45
soma <macaronieyes@gmail.com>
2024-01-22 23:35:27 +0900

    feat: 로고 아이콘 추가

--- client/src/components/MainNavigation.js
+++ client/src/components/MainNavigation.js
@@ -1,41 +1,37 @@
 import { Link } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { AnimatePresence } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faComment } from "@fortawesome/free-regular-svg-icons";
+import { useSelector } from "react-redux";
 
 import styles from "./MainNavigation.module.css";
 import Button from "./UI/Button";
-import Modal from "./UI/Modal";
-import AuthenticationPage from "../pages/Authentication";
-import { uiActions } from "../store/ui-slice";
+import Authentication from "./auth/Authentication";
+import Logout from "./auth/Logout";
 
 const MainNavigation = () => {
-  const dispatch = useDispatch();
-  const authIsVisible = useSelector((state) => state.ui.authIsVisible);
-
-  const handleLoginClick = () => {
-    dispatch(uiActions.setAuthIsVisible(true));
-  };
-
-  const handleConfirm = () => {
-    dispatch(uiActions.setAuthIsVisible(false));
-  };
+  const { nickname } = useSelector((state) => state.user.userInfo);
 
   return (
-    <>
-      <header className={styles.header}>
+    <header className={styles["main-nav"]}>
+      <div className={styles["header-box"]}>
         <h1>
-          <Link to="/">yop</Link>
+          <FontAwesomeIcon icon={faComment} className={styles["logo-icon"]}/>
+          <Link to="/">Yop</Link>
         </h1>
-        <Button onClick={handleLoginClick}>Log In</Button>
-      </header>
-      <AnimatePresence>
-        {authIsVisible && (
-          <Modal onConfirm={handleConfirm}>
-            <AuthenticationPage />
-          </Modal>
-        )}
-      </AnimatePresence>
-    </>
+        <div>
+          {nickname && (
+            <Logout>
+              <Button>LogOut</Button>
+            </Logout>
+          )}
+          {!nickname && (
+            <Authentication>
+              <Button>Log In</Button>
+            </Authentication>
+          )}
+        </div>
+      </div>
+    </header>
   );
 };
 
