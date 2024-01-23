45c7f144fbc18bcb6a55f85cba88a611896cc5c2
soma <macaronieyes@gmail.com>
2024-01-23 21:18:15 +0900

    feat: 페이지 이동시 애니메이션

--- client/src/pages/Root.js
+++ client/src/pages/Root.js
@@ -1,18 +1,34 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
+import { motion } from "framer-motion";
 
-import MainNavigation from '../components/MainNavigation';
+import styles from "./Root.module.css";
+import MainNavigation from "../components/MainNavigation";
+import SideNavigation from "../components/SideNavigation";
 
 function RootLayout() {
   // const navigation = useNavigation();
 
   return (
-    <>
+    <section className={styles["root-section"]}>
       <MainNavigation />
       <main>
         {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
-        <Outlet />
+        <motion.div
+          initial={{ opacity: 0, x: -50 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -50 }}
+        >
+          <Outlet />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: 50 }}
+        >
+          <SideNavigation />
+        </motion.div>
       </main>
-    </>
+    </section>
   );
 }
 
