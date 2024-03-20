8c2149b70236b15fa6da69cfe42a56a4dd3edba2
soma <macaronieyes@gmail.com>
2024-03-20 23:35:15 +0900

    feat: index

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./store";
+import { queryClient } from "./util/http";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>
 );
