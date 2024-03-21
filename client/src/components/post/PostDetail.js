4b8579b5f0964a6febd5d0cf1ea0a0d24677cffe
soma <macaronieyes@gmail.com>
2024-03-22 03:11:48 +0900

    feat: post detail

---
+++ client/src/components/post/PostDetail.js
@@ -0,0 +1,44 @@
+import { useLayoutEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+
+import PostItem from "./PostItem";
+import ModalHeader from "../UI/ModalHeader";
+import { useSelector } from "react-redux";
+
+function PostDetail({ onConfirm }) {
+  const [searchParams] = useSearchParams();
+  const modalIsVisible = useSelector((state) => state.ui.modalIsVisible);
+  const { data } = useQuery({
+    queryKey: ["post"],
+  });
+
+  const postId = searchParams.get("id");
+
+  const postData = (data?.data || []).find(
+    (el) => Number(el.id) === Number(postId)
+  );
+
+  useLayoutEffect(() => {
+    document.documentElement.style.scrollbarGutter = "stable";
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.documentElement.style.scrollbarGutter = "auto";
+      document.body.style.overflow = "visible";
+    };
+  }, []);
+
+  return (
+    <>
+      {modalIsVisible && <ModalHeader onConfirm={onConfirm} />}
+      <PostItem
+        content={postData.content}
+        nickname={postData.nickname}
+        title={postData.title}
+        createDate={postData.createdAt}
+      />
+    </>
+  );
+}
+
+export default PostDetail;
